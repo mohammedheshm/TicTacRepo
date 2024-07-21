38cd97cfb3c0a102715338416cab174881b7c332
Mohammed Hesham <mohammedhe4am@gmail.com>
2024-07-21 16:44:51 +0300

    Add new method printHelloGit2 in helloiti class

--- HelloITI.java
+++ HelloITI.java
@@ -6,5 +6,9 @@ public class HelloITI{
      public void printHelloGit() {
         System.out.println("New Method From ITI CLass , Fix Error in this Method");
     }
-		
+
+
+ public void printHelloGit2() {
+        System.out.println("New Method From ITI CLass");
+    }		
  }
